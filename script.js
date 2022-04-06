4066a96b7f59c007a0d826ff4bd909b9de42b43d
abi-widiarta <abiwidiarta03@gmail.com>
2022-04-06 15:49:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,142 @@
+const canvasItem0 = document.getElementsByClassName("canvas-item")[0];
+const canvasItem1 = document.getElementsByClassName("canvas-item")[1];
+const canvasItem2 = document.getElementsByClassName("canvas-item")[2];
+const canvasItem3 = document.getElementsByClassName("canvas-item")[3];
+const canvasItem4 = document.getElementsByClassName("canvas-item")[4];
+const canvasItem5 = document.getElementsByClassName("canvas-item")[5];
+const canvasItem6 = document.getElementsByClassName("canvas-item")[6];
+const canvasItem7 = document.getElementsByClassName("canvas-item")[7];
+const canvasItem8 = document.getElementsByClassName("canvas-item")[8];
+const canvasItem9 = document.getElementsByClassName("canvas-item")[9];
+const canvasItem10 = document.getElementsByClassName("canvas-item")[10];
+const canvasItem11 = document.getElementsByClassName("canvas-item")[11];
+const canvasItem12 = document.getElementsByClassName("canvas-item")[12];
+const canvasItem13 = document.getElementsByClassName("canvas-item")[13];
+const canvasItem14 = document.getElementsByClassName("canvas-item")[14];
+const canvasItem15 = document.getElementsByClassName("canvas-item")[15];
+const canvasItem16 = document.getElementsByClassName("canvas-item")[16];
+const canvasItem17 = document.getElementsByClassName("canvas-item")[17];
+const canvasItem18 = document.getElementsByClassName("canvas-item")[18];
+const canvasItem19 = document.getElementsByClassName("canvas-item")[19];
+const canvasItem20 = document.getElementsByClassName("canvas-item")[20];
+const canvasItem21 = document.getElementsByClassName("canvas-item")[21];
+const canvasItem22 = document.getElementsByClassName("canvas-item")[22];
+const canvasItem23 = document.getElementsByClassName("canvas-item")[23];
+const canvasItem24 = document.getElementsByClassName("canvas-item")[24];
+
+function ubahWarna(canvasItemColor, canvasItem) {
+  if (canvasItemColor == "white") {
+    canvasItem.style.backgroundColor = "black";
+  } else {
+    canvasItem.style.backgroundColor = "white";
+  }
+}
+
+canvasItem0.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem0.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem0);
+});
+canvasItem1.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem1.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem1);
+});
+canvasItem2.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem2.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem2);
+});
+canvasItem3.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem3.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem3);
+});
+canvasItem4.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem4.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem4);
+});
+canvasItem5.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem5.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem5);
+});
+canvasItem6.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem6.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem6);
+});
+canvasItem7.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem7.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem7);
+});
+canvasItem8.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem8.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem8);
+});
+canvasItem8.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem8.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem8);
+});
+canvasItem8.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem8.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem8);
+});
+canvasItem9.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem9.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem9);
+});
+canvasItem10.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem10.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem10);
+});
+canvasItem11.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem11.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem11);
+});
+canvasItem12.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem12.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem12);
+});
+canvasItem13.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem13.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem13);
+});
+canvasItem14.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem14.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem14);
+});
+canvasItem15.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem15.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem15);
+});
+canvasItem16.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem16.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem16);
+});
+canvasItem17.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem17.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem17);
+});
+canvasItem18.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem18.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem18);
+});
+canvasItem19.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem19.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem19);
+});
+canvasItem20.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem20.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem20);
+});
+canvasItem21.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem21.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem21);
+});
+canvasItem22.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem22.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem22);
+});
+canvasItem23.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem23.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem23);
+});
+canvasItem24.addEventListener("mousedown", function () {
+  const canvasItemColor = canvasItem24.style.backgroundColor;
+  ubahWarna(canvasItemColor, canvasItem24);
+});
